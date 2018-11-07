52860cd210005715f98e76b95d98799e010f670d
Marcel Mokos <marcel.mokos@gmail.com>
2018-11-07 21:24:52 +0100

    create example script and update example

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 gist="https://gist.githubusercontent.com/marcelmokos/8cb21782167f66847eb739790f2f0a06/raw"
 
-bash <(curl -s -H 'Cache-Control: no-cache' "$gist/eslint-config-with-prettier.sh")
+bash <(curl -s -H 'Cache-Control: no-cache' "$gist/eslint-config-with-prettier.sh") $1
